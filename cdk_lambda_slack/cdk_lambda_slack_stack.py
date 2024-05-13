21c30a2f7a2ad22f7f1f04d2ee642db5ef01eb47
Anastasia Filatova <anastasia.n.filatova@gmail.com>
2024-05-13 13:25:55 +0100

    Create lambda function and cdk stack

--- cdk_lambda_slack/cdk_lambda_slack_stack.py
+++ cdk_lambda_slack/cdk_lambda_slack_stack.py
@@ -1,19 +1,41 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_apigateway  as apigw,
 )
 from constructs import Construct
+from dotenv import load_dotenv
+import os
 
 class CdkLambdaSlackStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Load all environment variables from .env file into os.environ
+        load_dotenv()
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkLambdaSlackQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Defines a lambda function
+        lambda_fn = _lambda.Function(
+            self, "LambdaSlackHandler",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="lambda_handler.lambda_handler",
+            code=_lambda.Code.from_asset("lambda"),
+            environment={
+                'SLACK_SIGNING_SECRET': os.environ['SLACK_SIGNING_SECRET'],
+                'SLACK_BOT_TOKEN': os.environ['SLACK_BOT_TOKEN']
+            }
+        )
+
+        # Define API Gateway
+        api = apigw.LambdaRestApi(
+            self, "Endpoint",
+            handler=lambda_fn,
+            description="API Gateway for Slack bot",
+        )
+
+        # Define /slack/events resource path
+        #slack_events_resource = api.root.add_resource("slack").add_resource("events")
+
+        # Add POST method to /slack/events
+        #slack_events_resource.add_method("POST", integration=apigw.LambdaIntegration(lambda_fn)) 
---
+++ lambda/lambda_handler.py
@@ -0,0 +1,31 @@
+import os
+import json
+from dotenv import load_dotenv
+from slack_bolt import App
+
+# Load all environment variables from .env file into os.environ
+load_dotenv()
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+def lambda_handler(event, context):
+    print('request: {}'.format(json.dumps(event)))
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
+    }
+
+# Listens to incoming messages that contain "hello"
+# To learn available listener arguments,
+# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
