9c3c3bfd15478c7914e0c7c54b709775f21de711
Anastasia Filatova <anastasia.n.filatova@gmail.com>
2024-05-21 13:23:11 +0100

    Changing the way how lambda handle slack verification according to a slack bolt documentation.
    Listening and responding to a message using slack bolt event method.

--- lambda/lambda_handler.py
+++ lambda/lambda_handler.py
@@ -2,30 +2,27 @@ import os
 import json
 from dotenv import load_dotenv
 from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+
 
 # Load all environment variables from .env file into os.environ
 load_dotenv()
 
 # Initializes your app with your bot token and signing secret
-app = App(
-    token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
-)
+# process_before_response must be True when running on FaaS
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"),
+          signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+          process_before_response=True)
+
+# Listens to all incoming messages and says something back
+# To learn available event types,
+# visit https://api.slack.com/events?query=me&filter=Events
+@app.event("message")
+def handle_user_message(body, say):
+    print('body: {}'.format(json.dumps(body)))
+    text = body["event"]["text"]
+    say(text)
 
 def lambda_handler(event, context):
-    print('request: {}'.format(json.dumps(event)))
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'text/plain'
-        },
-        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
-    }
-
-# Listens to incoming messages that contain "hello"
-# To learn available listener arguments,
-# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
-@app.message("hello")
-def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    say(f"Hey there <@{message['user']}>!")
+    slack_handler = SlackRequestHandler(app=app)
+    return slack_handler.handle(event, context)
